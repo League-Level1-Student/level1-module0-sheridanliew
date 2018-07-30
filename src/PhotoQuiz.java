56fb0d9958d94d020b3c6996f26aae84244593c1
League <League@adminisorsimac2.attlocal.net>
2018-07-30 12:04:52 -0700

    Photo Quiz (incomplete) My First Swing GUI

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.awt.Component;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,25 +18,33 @@ public class PhotoQuiz {
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
+		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will make sure the program exits when you
+																	// close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+		// 1. find an image on the internet, and put its URL in a String variable (from
+		// your browser, right click on the image, and select “Copy Image Address”)
+		String imageURL1 = ("http://www.myiconfinder.com/uploads/iconsets/256-256-1ff6e565176b38899fd667fd744a2668-emoticons.png");
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component image;
+		image = createImage(imageURL1);
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
-
+		quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String answer=JOptionPane.showInputDialog("What is the face doing?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(answer.equals("smiling")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!!!");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
-		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT. The answer was 'smiling'.");
+		}
+		// 9. remove the component from the quiz window (you may not see the effect of
+		// this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
 
@@ -47,7 +54,7 @@ public class PhotoQuiz {
 
 		// 13. ask another question
 
-		// 14+ check answer, say if correct or incorrect, etc.
+		// 14. check answer, say if correct or incorrect, etc.
 
 	}
 
@@ -60,10 +67,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
