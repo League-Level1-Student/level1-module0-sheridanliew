9872cc1685d09f37120bec847e7ff0533c800d41
League <League@adminisorsimac2.attlocal.net>
2018-07-31 11:59:53 -0700

    Drum Kit (incomplete) Photo Quiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -23,10 +24,10 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String variable (from
 		// your browser, right click on the image, and select “Copy Image Address”)
-		String imageURL1 = ("http://www.myiconfinder.com/uploads/iconsets/256-256-1ff6e565176b38899fd667fd744a2668-emoticons.png");
+		String imageURL = ("http://www.myiconfinder.com/uploads/iconsets/256-256-1ff6e565176b38899fd667fd744a2668-emoticons.png");
 		// 2. create a variable of type "Component" that will hold your image
 		Component image;
-		image = createImage(imageURL1);
+		image = createImage(imageURL);
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
@@ -34,9 +35,9 @@ public class PhotoQuiz {
 		// 5. call the pack() method on the quiz window
 		quizWindow.pack();
 		// 6. ask a question that relates to the image
-		String answer=JOptionPane.showInputDialog("What is the face doing?");
+		String answer = JOptionPane.showInputDialog("What is the face doing?");
 		// 7. print "CORRECT" if the user gave the right answer
-		if(answer.equals("smiling")) {
+		if (answer.equals("smiling")) {
 			JOptionPane.showMessageDialog(null, "CORRECT!!!");
 		}
 		// 8. print "INCORRECT" if the answer is wrong
@@ -45,17 +46,23 @@ public class PhotoQuiz {
 		}
 		// 9. remove the component from the quiz window (you may not see the effect of
 		// this until step 12)
-
+		quizWindow.remove(image);
 		// 10. find another image and create it (might take more than one line of code)
-
+		String imageURL2 = ("https://emojipedia-us.s3.amazonaws.com/thumbs/120/facebook/65/loudly-crying-face_1f62d.png");
+		Component image2;
+		image2 = createImage(imageURL2);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(image2);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		JOptionPane.showInputDialog("What is the face doing?");
 		// 14. check answer, say if correct or incorrect, etc.
-
+		if (answer.equals("smiling")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!!!");
+		} else {
+			JOptionPane.showMessageDialog(null, "INCORRECT. The answer was 'crying'.");
+		}
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
