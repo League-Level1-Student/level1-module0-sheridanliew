dc4606c2d5b721e80570bdc9ef8ca7c15b9323e5
League <League@adminisorsimac2.attlocal.net>
2018-08-02 11:56:16 -0700

    Houses (incomplete)

--- src/Houses.java
+++ src/Houses.java
@@ -1,14 +1,18 @@
 
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
+	static Robot rbt = new Robot();
+	static Random random = new Random();
+	static String small = ("60");
+	static String medium = ("120");
+	static String large = ("250");
 
 	public static void main(String[] args) {
-
 		// 1. Have a robot start on the left side of the window near the bottom.
-		Robot rbt = new Robot();
-		rbt.setSpeed(10);
+		rbt.setSpeed(25);
 		rbt.moveTo(100, 500);
 		// 2. Draw a flat-topped house of height 100 with grass after it (see picture).
 		rbt.penDown();
@@ -19,13 +23,25 @@ public class Houses {
 		rbt.turn(90);
 		rbt.move(100);
 		rbt.turn(270);
-		rbt.setPenColor(7, 100, 0);
+		rbt.setPenColor(30, 500, 0);
 		rbt.move(15);
+		rbt.turn(270);
 		// 3. Extract the piece of code that draws the house into a method. Draw 10
 		// houses.
 
+		// for (int i = 0; i < 9; i++) {
+		// flatHouse(100);
+		// }
+
 		// 4. Change the method to take (int height) as a parameter. Draw 9 houses of
 		// different heights
+		if (random.equals(small)) {
+			drawPointyRoof(60);
+		} else if (random.equals(medium)) {
+			drawFlatRoof(120);
+		} else {
+			drawFlatRoof(250);
+		}
 
 		// 5. Make the method take a String instead of an int.
 		// “small” 60
@@ -45,4 +61,33 @@ public class Houses {
 		// 9. Make large houses have flat roofs
 
 	}
+
+	public static void drawFlatRoof(int height) {
+		rbt.setRandomPenColor();
+		rbt.move(height);
+		rbt.turn(90);
+		rbt.move(30);
+		rbt.turn(90);
+		rbt.move(height);
+		rbt.turn(270);
+		rbt.setPenColor(30, 500, 0);
+		rbt.move(15);
+		rbt.turn(270);
+	}
+
+	public static void drawPointyRoof(int height) {
+		rbt.setRandomPenColor();
+		rbt.move(height);
+		rbt.turn(45);
+		rbt.move(20);
+		rbt.turn(90);
+		rbt.move(20);
+		rbt.turn(45);
+		rbt.move(height);
+		rbt.turn(270);
+		rbt.setPenColor(30, 500, 0);
+		rbt.move(15);
+		rbt.turn(270);
+	}
+
 }
